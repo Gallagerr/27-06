c36915d0bb4aa0651a34f61d8e37c227e935614c
sadiev <sadiev@itstep.kz>
2019-06-17 21:40:03 +0600

    Добавьте файлы проекта.

---
+++ 27.06/DescriptionGame.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _27._06
+{
+  /// <summary>
+  /// Логика взаимодействия для DescriptionGame.xaml
+  /// </summary>
+  public partial class DescriptionGame : Window
+  {
+    public DescriptionGame()
+    {
+      InitializeComponent();
+    }
+  }
+}
